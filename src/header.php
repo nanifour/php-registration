f4f4f9d187b946611982b23a2e4a52c1e28dfadc
nanifour <nholloway013@gmail.com>
2021-05-25 21:45:42 -0400

    added bootstrap css to project

--- src/header.php
+++ src/header.php
@@ -8,7 +8,14 @@
   <title> Royal Pups FC </title> 
   <link rel="stylesheet" type="text/css" href="style/std.css">
   <meta charset = "UTF-8" />
-  
+
+    <!-- Bootstrap CSS -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
 </head>
 
@@ -38,7 +45,7 @@
           <a href="events.php"> Events </a> 
           <a href="index.php"> Log In </a> 
           <a href="logout_action.php"> Log Out </a> 
-          <a class="nav-link" href="registration.php"> Sign Up </a> 
+          <a href="registration.php"> Sign Up </a> 
       </div>
 
 
