5b2f7fcef10c339c59c68a13483baf8f14684032
nanifour <nholloway013@gmail.com>
2021-05-26 04:17:59 -0400

    added styling to the new events table

--- src/new_event.php
+++ src/new_event.php
@@ -10,75 +10,93 @@
 	<title> Royal Pups FC </title>
   <meta charset = "UTF-8" />
 
-  <style>
-form {
-  background-color: #56CBF9 !important;
-
-}
-
-.btn {
-  padding: 13px;
-  font-size: 15px;
-  color: white;
-  margin: 10px 0px 0px 215px;
-  background: #76178D;
-  border: none;
-  border-radius: 5px;
-}
-
-.btn:hover{
-  background-color: #45a049;
-}
-
-</style>
 </head>
 
 <body>
     <!-- Included Files -->
     <?php include("header.php"); ?>
 
-<?php
-//check if user is logged in before adding event
-if(empty($_SESSION['logged_in']) || ! $_SESSION['logged_in']){
-  //direct user to log in page if not logged in
-   header("Location: index.php");
-   exit;
-}
+  <div class="background">
 
-?>   
+    <div class="content">
+
+            <?php
+            //check if user is logged in before adding event
+            if(empty($_SESSION['logged_in']) || ! $_SESSION['logged_in']){
+              //direct user to log in page if not logged in
+              header("Location: index.php");
+              exit;
+            }
+
+            ?>  
+            
+            
+		    <div class="item-container">
+
+              <div class="header">
+                <div class="circle-container">
+                  <div class="circle-one"></div>
+                  <div class="circle-two"></div>
+                  <div class="circle-three"></div>
+                </div>
+                <div class="line"></div>
+              </div>
 
 
-<form method="post" action="new_event_action.php">
-  	<div class="input-group">
-  	  <label> Event Name </label>
-  	  <input type="text" placeholder="Name of the event.." name="event_name" required/>
-  	</div>
-    <div class="input-group">
-  	  <label> Sponsor </label>
-  	  <input type="text" placeholder="Sponsor of the event.."  name="sponsor" required/>
-  	</div> 
-  	<div class="input-group">
-  	  <label> Description </label>
-      <input type="text" name="description" placeholder="Enter short description of event..." required>
-  	</div>
-      <div class="input-group">
-  	  <label> Date </label>
-      <input type="date" name="date1" min=
-        <?php
-          echo date('Y-m-d');
-        ?>
-      >
-  	</div>
-    <div class="input-group">
-  	  <label> Time </label>
-  	  <input type="time" name="time1">
-  	</div>  
+              <div class="form-container">
 
-  	<div class="input-group">
-      <button type="submit" class="btn" name="register"> Add Event </button>
+                  <div class="text-header">
+                    <h4> Add An Event 🗓</h4>
+                  </div>	
+
+                  <form name="newEvent" method="post" action="new_event_action.php">
+
+                  <div class="format">
+                      <div class="form-floating mb-3">
+                          <input type="text" class="form-control" placeholder="Event Name" name="event_name" required/>
+                        <label for="floatingInput">Event Name</label>
+                      </div>
+                   </div>
+
+                   <div class="format">
+                      <div class="form-floating mb-3">
+                          <input type="text" class="form-control" placeholder="Sponsor"  name="sponsor" required/>
+                        <label for="floatingInput">Sponsor</label>
+                      </div> 
+                    </div>
+
+                      <div class="form-floating mb-3">
+                          <input type="text" class="form-control" name="description" placeholder="Short Description" required>
+                        <label for="floatingInput">Short Description</label>
+                      </div>
+
+
+                    <div class="dt-format">
+                        <label> Date </label>
+                            <input type="date" name="date1" min=
+                              <?php
+                                echo date('Y-m-d');
+                              ?>
+                            >
+
+                    </div>
+
+                    <div class="dt-format">
+                        <label> Time </label>
+                        <input type="time" name="time1">
+                    </div> 
+
+                      <div class="form-floating">
+                        <button type="submit" class="btn btn-primary" name="register"> Add Event </button>
+                      </div>
+                  </form>
+                </div>
+
+        </div>
+     
+      </div>
+  
     </div>
-    
-</form>
 
 
 
