771c92abd4c15ad131e76a88aa90b62c3416e63f
nanifour <nholloway013@gmail.com>
2021-05-26 02:19:40 -0400

    styled registration page

--- src/registration.php
+++ src/registration.php
@@ -10,78 +10,103 @@
     <meta charset = "UTF-8" />
 
   <script src="app.js"></script>
+<style>
+.form-container{
+  padding: 1vh;
+ 
+}
+</style>
 
 </head>
 
-<body>	
-			
+<body>			
 
-	<div class="signUp-container">
+	<div class="background">
 
+		<div class="content">
 			<?php  
 				//show session message
 				if (isset($_SESSION['message'])){
 
-					//boostrap alert
+				//boostrap alert
 					?>
 						<div class="alert alert-primary" role="alert">
-							<strong>Hey!</strong> <?php echo  $_SESSION['message'];?>
+							<strong>Welcome, </strong> <?php echo  $_SESSION['message'];?>
 						</div>
-					<?php
+						<?php
 						//clear session message after
 						unset($_SESSION['message']);
 				}
 			?>
 
+			<div class="item-container">
 
-  		<div class="title">
-			<h3> Sign up to become a fan member for more information, free gear, and more from your favorive soccer club! </h3>
+				<div class="header">
+					<div class="circle-container">
+						<div class="circle-one"></div>
+						<div class="circle-two"></div>
+						<div class="circle-three"></div>
+					</div>
+					<div class="su-line"></div>
+			</div>
+
+			<div class="form-container">
+						<div class="text-header">
+							<h4> Sign up to become a Royal Pup 🐶⚽️! </h4>
+						</div>	
+
+
+				<form name="registerForm" method="POST" action="registration_action.php">
+
+					<div class="format">
+						<div class="form-floating mb-3">
+								<input type="text" class="form-control input-sm" placeholder="Full Name" name="name" required/>
+							<label for="input-sm">Full Name </label>
+						</div>
+					</div>
+					<div class="format">
+						<div class="form-floating mb-3">
+								<input type="text" class="form-control" placeholder="Username" name="username" required/>
+							<label for="floatingInput">Username </label>
+						</div>  
+					</div>
+
+						<div class="form-floating mb-3">							
+								<input type="email" class="form-control" placeholder="Email" name="email" required/>
+							<label for="floatingInput">Email </label>
+						</div> 
+
+						<div class="form-floating">
+									<input type="password" class="form-control" placeholder="Password" id="password1" name="password_1" required/>
+								<label for="floatingInput">Password </label>
+						</div>
+
+						<div class="form-floating mb-3">
+									<input type="password" class="form-control" placeholder="Confirm Password" id="password2"
+									onkeyup = checkPassword(); name="password_2" required/>
+									<input type="checkbox" onclick="showPasswords()"/> Show Passwords
+								<label for="floatingInput">Confirm password </label>
+						</div>
+
+						<div class="form-floating mb-3">
+								<input type="text" class="form-control" placeholder="5 digit Zip Code" name="zipcode" required/>
+							<label for="floatingInput">Zip Code </label>
+						</div>
+						
+					<div class="form-floating">
+						<button type="submit" class="btn btn-primary" name="register"> Sign Up </button>
+					</div>
+
+						<p>Already a member? 
+							<a href="index.php">Log In</a>
+						</p>
+				
+				</form>
+			</div>
+
 		</div>
 
-			<form name="registerForm" method="POST" action="registration_action.php">
-				<div class="signup">
-					<label>Name </label>
-						<input type="text" placeholder="Full Name.." name="name" required/>
-				</div>
-				<div class="signup">
-					<label>Username </label>
-						<input type="text" placeholder="Username.." name="username" required/>
-				</div>   
-				<div class="signup">
-					<label>Email </label>
-						<input type="email" placeholder="Email Address.." name="email" required/>
-				</div> 
-				<div class="signup">
-					<label>Password </label>
-							<input type="password" placeholder="Password.." id="password1" name="password_1" required/>
-				</div>
-				<div class="signup">
-					<label>Confirm password </label>
-							<input type="password" placeholder="Confirm Password.." id="password2"
-							onkeyup = checkPassword(); name="password_2" required/>
-							<input type="checkbox" onclick="showPasswords()"/> Show Passwords
-				</div>
-				<div class="signup">
-					<label>Zip Code </label>
-						<input type="text" placeholder="5 digit Zip Code .." name="zipcode" required/>
-				</div>
-
-			<p>By signing up to become a fan member, you agree to our <a href="#" style="color: blue">Terms & Conditions</a>.</p>
-
-				
-			<div class="signup">
-			<button type="submit" class="btn" name="register"> Sign Up </button>
-			</div>
-
-			<div class="form-footer">
-				<p>Already a Royal Pups fan member? <a href="index.php" style="color: blue">Log In</a>.</p>
-			</div>
-		
-		</form>
-
-
 	</div>
-
 
 
 </body>
