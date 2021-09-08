41ca558aaf5b819342ae68a139a65979fd636731
nanifour <nholloway013@gmail.com>
2021-09-08 17:58:40 -0400

    favicons added

--- include/header.php
+++ include/header.php
@@ -10,6 +10,11 @@
   <script src="include/app.js"></script>
   <meta charset = "UTF-8" />
 
+  <link rel="shortcut icon" type="image/png" href="/images/favicon.ico"/>
+  <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+
     <!-- Bootstrap CSS -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
